70078f8d11bee38773ccd3f7b9186152ecd0ac96
Ivan Khodyrev <hodyrev.i@mail.ru>
2019-10-09 14:37:22 +0300

    SocketWorker fixed. Send and recieve

--- StandartCommandInterpreter.cpp
+++ StandartCommandInterpreter.cpp
@@ -21,7 +21,7 @@
 			char *b  = new char[cmd->argv[0].size()];
 			strcpy(b, cmd->argv[0].c_str());
 			string tmp = to_string(StadnartCommandInterpreter::dw->GetHash(b));
-			char p[tmp.size()];
+			char p[tmp.size()+1];
 			strcpy(p, tmp.c_str());
 			StadnartCommandInterpreter::sw->Send(sock, p, sizeof(p));
 		}else
